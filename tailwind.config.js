bb26c31cfc05a69d284721b90d0569db206a117d
Israel Ikechukwu <85766039+IsraelIyke@users.noreply.github.com>
2025-12-28 20:28:48 +0100

    Enhance tailwind.config.js with fadeIn animation
    Added custom animation and keyframes for fadeIn effect.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,10 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+      },
+       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
       },
     },
   },
